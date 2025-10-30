10c94d02d8f48029ac0efd216b941ea15c814c92
Platylaza <smellfire32@outlook.com>
2025-10-30 17:44:37 -0600

    Update layout.tsx

--- app/recipes/layout.tsx
+++ app/recipes/layout.tsx
@@ -3,7 +3,7 @@ import Navbar from "@/app/ui/navbar";
 export default function Layout({ children }: { children: React.ReactNode }) {
 	return (
 		<div>
-			<Navbar>{}</Navbar>
+			<Navbar>{<></>}</Navbar>
 			<div className="flex-grow p-6">{children}</div>
 		</div>
 	);
