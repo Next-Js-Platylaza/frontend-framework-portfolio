b58eb73bf3ac75064253ed0f99604886bc92f82e
Web Development <web@btech.edu>
2025-11-06 12:54:30 -0700

    Added suspence to account/create/page.tsx for Vercel

--- app/account/create/page.tsx
+++ app/account/create/page.tsx
@@ -1,9 +1,10 @@
 import CreateAccountForm from "@/app/ui/account/create-form";
+import { Suspense } from "react";
 
 export default function Page() {
 	return (
-		<>
+		<Suspense>
 			<CreateAccountForm />
-		</>
+		</Suspense>
 	);
 }
